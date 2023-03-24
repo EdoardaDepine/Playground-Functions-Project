46cd8cfa190a0cb41c16eaec8c343dc5811df96c
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-24 17:18:45 -0300

    desafio 3 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -10,3 +10,12 @@ function calcArea(base, height) {
   const resultado = (base * height) / 2;
   return resultado;
 }
+
+// Desafio 3
+const goTrybe = "go tyrbe";
+const vamoQueVamo = "vamo que vamo";
+const foguete = "foguete";
+
+function splitSentence(parametro) {
+  return parametro.split(" ");
+}
