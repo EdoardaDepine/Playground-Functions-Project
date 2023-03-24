078abe483ad85bf7a6e5fcd0097545c447668f0d
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-24 17:18:01 -0300

    desafio 2 concluído

---
+++ src/challenges.js
@@ -0,0 +1,12 @@
+// Desafio 1
+function compareTrue(boolean1, boolean2) {
+  if (boolean1 && boolean2) {
+    return true;
+  }
+  return false;
+}
+// Desafio 2
+function calcArea(base, height) {
+  const resultado = (base * height) / 2;
+  return resultado;
+}
